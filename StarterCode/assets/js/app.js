a97939d510c2b72a3b2d0e3fc561c45a57b249b5
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-12 19:33:30 -0700

    creating margins

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,14 @@
+// @TODO: YOUR CODE HERE!
+//svg container
+const svgHeight = 960
+const svgWidth = 1000
+//margins
+const margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  };
+
+  const height = svgHeight - margin.top - margin.bottom;
+  const width = svgWidth - margin.left - margin.right;
