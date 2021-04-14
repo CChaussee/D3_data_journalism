f23365d1d444af3cdf80977363d20be800328121
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-14 08:31:05 -0700

    appending svg

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -10,5 +10,10 @@ const margin = {
     left: 50
   };
 
-  const height = svgHeight - margin.top - margin.bottom;
-  const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+const width = svgWidth - margin.left - margin.right;
+
+const svg = d3.select(#scatter)
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
