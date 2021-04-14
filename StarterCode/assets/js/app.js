270225491e714a299401b3acde55a0b523f1db24
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-14 09:08:28 -0700

    creating xScale function

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -17,10 +17,17 @@ const svg = d3.select("#scatter")
     .append("svg")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
-
+// appending chartgroup
 const chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 //reading the csv  
 d3.csv("assets/data/data.csv").then(function(data) { 
   console.log(data);
-})    
+})
+// 
+function xScale(peopleData, "income") {
+  const xLinearScale = d3.scaleLinear()
+  .domain([d3.min(peopleData, d => d["income"]) * 0.8,
+    d3.max(peopleData, d => d["income"]) * 1.2])
+  .range([0, width]);
+}
