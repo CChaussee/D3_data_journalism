0e28f80dd03a47041bfc3077e2603685ebfa2965
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-15 14:40:12 -0700

    yaxis is now appearing

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -14,41 +14,37 @@ const width = svgWidth - margin.left - margin.right;
 
 const svg = d3.select("#scatter")
     .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
+      .attr("height", svgHeight)
+      .attr("width", svgWidth)
+    .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 // appending chartgroup
-const chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 function makeGraph(peopleData) {
   // xScale
-  xScale = d3.scaleLinear()
-  .domain([0,22])
-  .range([0, width])
-svg.append('g')
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(xScale));
+  const xScale = d3.scaleLinear()
+    .domain([0,22])
+    .range([0, width])
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale));
 //yscale
-  yScale = d3.scaleLinear() 
-  .domain([0, 28])
-  .range([height,0])
-svg.append("g")
-  .call(d3.axisLeft(yScale));
+  const yScale = d3.scaleLinear() 
+    .domain([0, 28])
+    .range([height,0])
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
 
 //Creating dots for scatter plot, thank you D3 graph gallery
-renderCircles =
-  svg.append('g')
-    .selectAll("dot")
-    .data(peopleData)
-    .enter()
-    .append("circle")
-    .attr("cx" , xScale)
-    .attr("cy", yScale)
-    .attr("r", 12)
-    .style("fill", "blue")
-
-updateToolTip =
-  d3.select("body").append("div")
-    .attr("class", "tooltip");
+  const renderCircles =
+    svg.append('g')
+      .selectAll("dot")
+      .data(peopleData)
+      .enter()
+      .append("circle")
+        .attr("cx" , xScale)
+        .attr("cy", yScale)
+        .attr("r", 12)
+        .style("fill", "blue")
 
 }
 
