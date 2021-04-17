6b9847ae9b8913188055efc330d4f459e2f63a76
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-17 09:53:10 -0700

    creating margins for the graph to sit in

---
+++ app.js
@@ -0,0 +1,8 @@
+// Creating margins for graph, thank you AskBCS
+// I wasnt creating boundaries for my graph to sit in orginally
+const width = parseInt(d3.select("#scatter").style("width"));
+const height = width - width / 4.0;
+const margin = 20
+const labelArea = 110;
+const bottomPad = 30;
+const leftPad = 30;
