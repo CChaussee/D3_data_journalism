c729e662f3118d43ec55e7a0834e64eef033fe08
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-17 10:26:11 -0700

    adding text

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// Creating margins for graph, thank you AskBCS
+//My orginal code was a hot mess so I am starting from scratch with the help of AskBCS
+// Creating margins for graph, thank you AskBCS, my first attempt was a huge graph
 // I wasnt creating boundaries for my graph to sit in orginally
 const width = parseInt(d3.select("#scatter").style("width"));
 const height = width - width / 4.0;
@@ -18,7 +19,15 @@ xText.append("text")
   .attr("y",-26)
   .attr("data-name","poverty")
   .attr("data-axis","x")
-  .attr("class","aTextactivex")
+  .attr("class","aText active x")
   .text("InPoverty(%)");
 svg.append("g").attr("class", "yText");
-const yText = d3.select(".yText");
+//Have to rotate text for y axis
+const yText = d3.select(".yText")
+  .attr("transform","translate("+leftTextX+","+leftTextY+")rotate(-90)")
+  .append("text")
+  .attr("y", -26)
+  .attr("data-name", "obesity")
+  .attr("data-axis", "y")
+  .attr("class", "aText active y")
+  .text("Obese (%)")
